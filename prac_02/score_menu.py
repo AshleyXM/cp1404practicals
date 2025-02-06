199c9f1beaffc0ab21b17f2bade476bf1afee2ec
AshleyXM <454198339@qq.com>
2025-02-06 21:00:29 +0800

    Add score_menu.py

---
+++ prac_02/score_menu.py
@@ -0,0 +1,50 @@
+"""
+Use menu to ask the user for a score and print the score status and stars
+"""
+MENU = """G - Get a valid score (must be 0-100 inclusive)
+P - Print result
+S - Show stars
+Q - Quit
+"""
+
+def main():
+    print(MENU)
+    # Get a valid score
+    score = get_valid_score()
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "G":
+            score = get_valid_score()
+        elif choice == "P":
+            print(determine_score(score))
+        elif choice == "S":
+            print(score * "*")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Goodbye")
+
+
+def get_valid_score():
+    """Get a valid score (0-100 inclusive)"""
+    score = int(input("Enter score: "))
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = int(input("Enter score: "))
+    return score
+
+
+def determine_score(score):
+    """Determine score status"""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
