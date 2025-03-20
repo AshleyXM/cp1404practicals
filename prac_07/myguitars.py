7911adef2eacfb062560731cc24ebec9b2649148
AshleyXM <454198339@qq.com>
2025-03-20 15:37:01 +0800

    Complete second part of More Guitars exercise

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -9,8 +9,13 @@ FILENAME = "guitars.csv"
 def main():
     """Main function to read and show guitars"""
     guitars = read_data()
-    guitars.sort()
+    guitar = get_guitar_info()
+    while guitar is not None:
+        guitars.append(guitar)
+        guitar = get_guitar_info()
+    # guitars.sort()
     display_data(guitars)
+    save_guitars(guitars)
 
 
 def read_data():
@@ -21,6 +26,7 @@ def read_data():
         parts = line.strip().split(",")
         guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
         guitars.append(guitar)
+    in_file.close()
     return guitars
 
 
@@ -28,6 +34,25 @@ def display_data(guitars):
     """Display data from guitars"""
     for guitar in guitars:
         print(guitar)
+
+
+def get_guitar_info():
+    """Get guitar info"""
+    name = input("Name: ")
+    if name == "":
+        return None
+    year = int(input("Year: "))
+    cost = float(input("Cost: $"))
+    new_guitar = Guitar(name, year, cost)
+    return new_guitar
+
+
+def save_guitars(guitars):
+    """Save the guitars into the file"""
+    out_file = open(FILENAME, "w")
+    for guitar in guitars:
+        out_file.write(f"{guitar.name},{guitar.year},{guitar.cost}\n")
+    out_file.close()
 
 
 main()
