ab5633ff48cf696d57ae00cadeb5eac53f4dedcb
AshleyXM <454198339@qq.com>
2025-04-04 22:05:50 +0800

    Implement handling drive function of taxi simulator task

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -3,17 +3,17 @@ from silver_service_taxi import SilverServiceTaxi
 
 def main():
     print("Let's drive!")
-    current_taxi = None
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2),
              SilverServiceTaxi("Hummer", 200, 4)]
+    current_taxi = None
     display_menu()
     choice = input(">>> ").lower()
     bill_amount = 0
-    while choice != 'q' and choice != 'quit':
+    while choice != 'q':
         if choice == 'c':
             pass
         elif choice == 'd':
-            pass
+            bill_amount += handle_drive(current_taxi)
         else:
             print("Invalid option")
         print(f"Bill to date: ${bill_amount:.2f}")
@@ -25,5 +25,18 @@ def display_menu():
     """Display the menu"""
     print("q)uit, c)hoose taxi, d)rive")
 
+
+def handle_drive(current_taxi):
+    """Drive the taxi"""
+    if current_taxi is None:
+        print("You need to choose a taxi before you can drive")
+        return 0
+    else:
+        distance = int(input("Drive how far? "))
+        current_taxi.drive(distance)
+        fare = current_taxi.get_fare()
+        current_taxi.start_fare()
+        print(f"Your {current_taxi.name} trip cost you ${fare:.2f}")
+        return fare
 
 main()
