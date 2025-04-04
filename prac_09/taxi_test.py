9a0f98316cd8a2a6c4be53bb18b4dbb0d9c48c12
AshleyXM <454198339@qq.com>
2025-04-04 14:36:55 +0800

    Improve class taxi by using a class variable

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -7,7 +7,7 @@
 """
 from prac_09.taxi import Taxi
 
-my_taxi = Taxi("Prius 1", 100, 1.23)
+my_taxi = Taxi("Prius 1", 100)
 my_taxi.drive(40)
 print(my_taxi)
 print(f"Current fare is {my_taxi.get_fare()}")
