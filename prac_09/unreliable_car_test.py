5e6bdb28b35d437365492c1972415bc706154f9f
AshleyXM <454198339@qq.com>
2025-04-04 15:10:10 +0800

    Finish UnreliableCar exercise

---
+++ prac_09/unreliable_car_test.py
@@ -0,0 +1,24 @@
+from unreliable_car import UnreliableCar
+
+ITERATION_TIMES = 100
+
+def main():
+    unreliable_car_a = UnreliableCar("Car A", 200, 30)
+    unreliable_car_b = UnreliableCar("Car B", 500, 60)
+    drive_count_a = 0
+    drive_count_b = 0
+    for _ in range(ITERATION_TIMES):
+        distance_driven_a = unreliable_car_a.drive(2)
+        print(f"{unreliable_car_a.name} has driven {distance_driven_a}km")
+        if distance_driven_a != 0:
+            drive_count_a += 1
+    for _ in range(ITERATION_TIMES):
+        distance_driven_b = unreliable_car_b.drive(4)
+        print(f"{unreliable_car_b.name} has driven {distance_driven_b}km")
+        if distance_driven_b != 0:
+            drive_count_b += 1
+    print(f"Total count: {ITERATION_TIMES}. Car A Drive count: {drive_count_a}")
+    print(f"Total count: {ITERATION_TIMES}. Car B Drive count: {drive_count_b}")
+
+
+main()
